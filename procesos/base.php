8990fbf10245528bc89489ea4ab5668872c2c01b
Willy Narvaez <willynarvaez6@gmail.com>
2014-09-23 11:53:54 -0500

    Cambios en la base para conexion con heroku

--- procesos/base.php
+++ procesos/base.php
@@ -3,7 +3,7 @@ function conectarse() {
 	if (!($conexion = pg_pconnect("dbname=df2jp28bdkuafd host=ec2-54-204-32-91.compute-1.amazonaws.com port=5432 user=larfyvwbaurpxo password=WV84lJxFXf7aqF6BXCXgwcI-tC sslmode=require"))) {			   
         exit();
     } else {   
-    echo "ok";      
+          
     }
     return $conexion;
 }
