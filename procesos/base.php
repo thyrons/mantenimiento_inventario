621d835358ced6478422ff2a88a085f004d5b29f
Willy Narvaez <willynarvaez6@gmail.com>
2014-09-23 11:30:16 -0500

    Cambios

--- procesos/base.php
+++ procesos/base.php
@@ -1,7 +1,7 @@
 <?php
 function conectarse() {
-	if (!($conexion = pg_connect("dbname=dcf3ce05b4dp26 host=ec2-54-225-101-4.compute-1.amazonaws.com port=5432 user=zxldypgcuhasxn password=cNiUIdbRg8zzlbfa2NupQbHZVy sslmode=require"))) {			
-    //if (!($conexion = pg_connect("dbname=mantenimiento_inventario port=5432 user=postgres password=root host=localhost"))) {			
+	if (!($conexion = pg_connect("dbname=dcf3ce05b4dp26 host=ec2-54-225-101-4.compute-1.amazonaws.com port=5432 user=zxldypgcuhasxn password=cNiUIdbRg8zzlbfa2NupQbHZVy sslmode=require") or die('La consulta fallo: ' . pg_last_error()))) {			
+    //if (!($conexion = pg_connect("dbname=mantenimiento_inventario port=5432 user=postgres password=root host=localhost") or die('La consulta fallo: ' . pg_last_error()))) {			
         exit();
     } else {         
     }
