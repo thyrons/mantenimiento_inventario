6acc40e8238f07df54b48e73211ca99ea8f1c2ba
Willy Narvaez <willynarvaez6@gmail.com>
2014-10-07 09:53:48 -0500

    correcion de errores
    guardar procesos clientes error al insertar corregido

--- procesos/guardar_clientes.php
+++ procesos/guardar_clientes.php
@@ -14,7 +14,9 @@ while ($row = pg_fetch_row($consulta)) {
 $cont++;
 /////////////////////////////////////////////////////////
 
-pg_query("insert into clientes values('$cont','$_POST[tipo_docu]','$_POST[ruc_ci]','$_POST[nombres_cli]','$_POST[tipo_cli]','$_POST[direccion_cli]','$_POST[nro_telefono]','$_POST[nro_celular]','$_POST[pais_cli]','$_POST[ciudad_cli]','$_POST[email]','$_POST[cupo_credito]','$_POST[notas_cli]','Activo'");
-$data = 1;
+if(pg_query("insert into clientes values('$cont','$_POST[tipo_docu]','$_POST[ruc_ci]','$_POST[nombres_cli]','$_POST[tipo_cli]','$_POST[direccion_cli]','$_POST[nro_telefono]','$_POST[nro_celular]','$_POST[pais_cli]','$_POST[ciudad_cli]','$_POST[email]','$_POST[cupo_credito]','$_POST[notas_cli]','Activo')")){
+	$data = 1;
+}
+
 echo $data;
 ?>
