29c851bc1100d70276d1a143e0f283282ada83c8
Oskrin <oskrs11@hotmail.com>
2014-10-07 16:36:38 -0500

    sesión
    cambio en sesión

--- reportes/reportes/reporteRegistro.php
+++ reportes/reportes/reporteRegistro.php
@@ -1,7 +1,8 @@
 <?php
+
 require('../dompdf/dompdf_config.inc.php');
 session_start();
-	$codigo='<html> 
+$codigo = '<html> 
     <head> 
    		<link rel="stylesheet" href="../../css/estilosReportes.css" type="text/css" /> 
 	</head> 
@@ -9,22 +10,22 @@ session_start();
 		<header>
 			<img src="../../images/logo_empresa.jpg" />
             <div id="me">
-                <h2 style="text-align:center;border:solid 0px;width:100%;">'.$_SESSION['empresa'].'</h2>
-                <h4 style="text-align:center;border:solid 0px;width:100%;">'.$_SESSION['slogan'].'</h4>
-                <h4 style="text-align:center;border:solid 0px;width:100%;">'.$_SESSION['propietario'].'</h4>
-                <h4 style="text-align:center;border:solid 0px;width:100%;">'.$_SESSION['direccion'].'</h4>
-                <h4 style="text-align:center;border:solid 0px;width:100%;">Telf: '.$_SESSION['telefono'].' Cel:  '.$_SESSION['celular'].' '.$_SESSION['pais_ciudad'].'</h4>
+                <h2 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['empresa'] . '</h2>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['slogan'] . '</h4>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['propietario'] . '</h4>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['direccion'] . '</h4>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">Telf: ' . $_SESSION['telefono'] . ' Cel:  ' . $_SESSION['celular'] . ' ' . $_SESSION['pais_ciudad'] . '</h4>
             </div>    
 		</header>        
 		<hr>
 		<div id="linea">
 			<h3>FORMULARIO DE INGRESO DE EQUIPO</h3>
 		</div>';
-		include '../../procesos/base.php';
-		conectarse();
-		$consulta = pg_query("select * from registro_equipo,color,marcas,clientes,usuario,categoria where registro_equipo.id_color=color.id_color and registro_equipo.id_marca=marcas.id_marca and registro_equipo.id_cliente=clientes.id_cliente and registro_equipo.id_usuario=usuario.id_usuario and registro_equipo.id_categoria=categoria.id_categoria and registro_equipo.id_registro='$_GET[id]'");   
-			while ($row = pg_fetch_row($consulta)) {	
-		$codigo.='<div id="cuerpo">		 
+include '../../procesos/base.php';
+conectarse();
+$consulta = pg_query("select * from registro_equipo,color,marcas,clientes,usuario,categoria where registro_equipo.id_color=color.id_color and registro_equipo.id_marca=marcas.id_marca and registro_equipo.id_cliente=clientes.id_cliente and registro_equipo.id_usuario=usuario.id_usuario and registro_equipo.id_categoria=categoria.id_categoria and registro_equipo.id_registro='$_GET[id]'");
+while ($row = pg_fetch_row($consulta)) {
+    $codigo.='<div id="cuerpo">		 
             <h2>Registro #</h2>                                    
             <h2>Fecha Salida</h2>                        
             <h2>Nro. Serie </h2>             
@@ -32,17 +33,17 @@ session_start();
        		</div>
         	<div id="medio">
             <h2>';
-            $codigo.=$row[0]." ";
-            $codigo.='</h2>                                    
+    $codigo.=$row[0] . " ";
+    $codigo.='</h2>                                    
             <h2>';
-            $codigo.=$row[12]." ";
-            $codigo.='</h2>
+    $codigo.=$row[12] . " ";
+    $codigo.='</h2>
             <h2>';
-            $codigo.=$row[4]."";
-            $codigo.='</h2>             
+    $codigo.=$row[4] . "";
+    $codigo.='</h2>             
             <h2 style="width:379px;height:13px;">';
-            $codigo.=$row[34]." ".$row[35] ;
-            $codigo.='</h2>          
+    $codigo.=$row[34] . " " . $row[35];
+    $codigo.='</h2>          
         	</div>
          	<div id="medio1">
             <h2>Cliente</h2>                                    
@@ -51,14 +52,14 @@ session_start();
         	</div>
         	<div id="medio2">
             <h2>';
-            $codigo.=$row[22]." ";
-            $codigo.='</h2>                                    
+    $codigo.=$row[22] . " ";
+    $codigo.='</h2>                                    
             <h2>';
-            $codigo.=$row[45]." ";
-            $codigo.='</h2>                        
+    $codigo.=$row[45] . " ";
+    $codigo.='</h2>                        
             <h2>';
-            $codigo.= $row[18]." ";
-            $codigo.='</h2>                                  
+    $codigo.= $row[18] . " ";
+    $codigo.='</h2>                                  
         	</div>
           	<div id="medio3">
             <h2>Fecha Entrada</h2>                                    
@@ -68,17 +69,17 @@ session_start();
         	</div>
         	<div id="medio4">
             <h2>';
-            $codigo.=$row[9]." ";
-            $codigo.='</h2>                                    
+    $codigo.=$row[9] . " ";
+    $codigo.='</h2>                                    
             <h2>';
-            $codigo.=$row[11]." ";
-            $codigo.='</h2>                        
+    $codigo.=$row[11] . " ";
+    $codigo.='</h2>                        
             <h2>';
-            $codigo.=$row[16]." ";
-            $codigo.='</h2>  
+    $codigo.=$row[16] . " ";
+    $codigo.='</h2>  
             <h2>';
-            $codigo.=$row[26]." ";
-            $codigo.='</h2>                           
+    $codigo.=$row[26] . " ";
+    $codigo.='</h2>                           
         	</div>
          	<div id="acce">
                 <h2>Accesorios</h2>                                                                                         
@@ -87,15 +88,15 @@ session_start();
         	</div>
          	<div id="acce1">            
                 <textarea name="">';
-                $codigo.=$row[6]." ";
-                $codigo.='</textarea>
+    $codigo.=$row[6] . " ";
+    $codigo.='</textarea>
                 <textarea name="">';
-                $codigo.=$row[5]." ";
-                $codigo.='</textarea>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;El diagnóstico del equipo tiene un precio mínimo de $10.00 (Diez Dólares)</h3>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Toda máquina reparada y no retirada en tres meses será subastada</h3>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Favor revisar que en su orden consten todos los componentes que usted deja. No se admitiran reclamos posteriores</h3>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;PyS Systems no se responsabiliza por la pérdida de la información</h3>
+    $codigo.=$row[5] . " ";
+    $codigo.='</textarea>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;El diagnóstico del equipo tiene un precio mínimo de $10.00 (Diez Dólares)</h3>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Toda máquina reparada y no retirada en tres meses será subastada</h3>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Favor revisar que en su orden consten todos los componentes que usted deja. No se admitiran reclamos posteriores</h3>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;PyS Systems no se responsabiliza por la pérdida de la información</h3>
                 
         	</div>
         	<div>
@@ -109,33 +110,33 @@ session_start();
             </div>    
         	<div id="resp">
         	<h2>';
-             $codigo.="Entrege conforme: ".$row[34]." ".$row[35] ;
-             $codigo.='</h2>                          
+    $codigo.="Entrege conforme: " . $row[34] . " " . $row[35];
+    $codigo.='</h2>                          
         	</div>   
             <div id="resp1">
-            <h2>';             
-             $codigo.="Recibi conforme: ".$row[22];
-             $codigo.='</h2>             
+            <h2>';
+    $codigo.="Recibi conforme: " . $row[22];
+    $codigo.='</h2>             
             </div>         
-			';	
-		}       
-        $codigo.='<div id="m"><div id="copia1">
+			';
+}
+$codigo.='<div id="m"><div id="copia1">
             <img src="../../images/logo_empresa.jpg" />
             <div id="meC">
-            <h2>P&S SYSTEMS</h2>
-                <h4>SERVICIOS INTEGRALES</h4>
-                <h4>YEPEZ RIVERA PABLO SANTIAGO</h4>
-                <h4>Dirección: Av. Eugenio Espejo 9-66 y Juan Fransico Bonilla Telf: 2603193</h4>
-                <h4>Web:www.pssystems-ec.com E-mail:ps.systems@hotmail.com IBARRA-ECUADOR</h4>   
+            <h2 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['empresa'] . '</h2>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['slogan'] . '</h4>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['propietario'] . '</h4>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">' . $_SESSION['direccion'] . '</h4>
+                <h4 style="text-align:center;border:solid 0px;width:100%;">Telf: ' . $_SESSION['telefono'] . ' Cel:  ' . $_SESSION['celular'] . ' ' . $_SESSION['pais_ciudad'] . '</h4>
             </div>                 
         </div>
         <hr>
         <div id="linea">
             <h3>FORMULARIO DE INGRESO DE EQUIPO</h3>
-        </div>';        
-        $consulta = pg_query("select * from registro_equipo,color,marcas,clientes,usuario,categoria where registro_equipo.id_color=color.id_color and registro_equipo.id_marca=marcas.id_marca and registro_equipo.id_cliente=clientes.id_cliente and registro_equipo.id_usuario=usuario.id_usuario and registro_equipo.id_categoria=categoria.id_categoria and registro_equipo.id_registro='$_GET[id]'");   
-        while ($row = pg_fetch_row($consulta)) {    
-            $codigo.='<div id="cuerpo">      
+        </div>';
+$consulta = pg_query("select * from registro_equipo,color,marcas,clientes,usuario,categoria where registro_equipo.id_color=color.id_color and registro_equipo.id_marca=marcas.id_marca and registro_equipo.id_cliente=clientes.id_cliente and registro_equipo.id_usuario=usuario.id_usuario and registro_equipo.id_categoria=categoria.id_categoria and registro_equipo.id_registro='$_GET[id]'");
+while ($row = pg_fetch_row($consulta)) {
+    $codigo.='<div id="cuerpo">      
             <h2>Registro #</h2>                                    
             <h2>Fecha Salida</h2>                        
             <h2>Nro. Serie </h2>             
@@ -143,17 +144,17 @@ session_start();
             </div>
             <div id="medioC">
             <h2>';
-            $codigo.=$row[0]." ";
-            $codigo.='</h2>                                    
+    $codigo.=$row[0] . " ";
+    $codigo.='</h2>                                    
             <h2>';
-            $codigo.=$row[12]." ";
-            $codigo.='</h2>
+    $codigo.=$row[12] . " ";
+    $codigo.='</h2>
             <h2>';
-            $codigo.=$row[4]."";
-             $codigo.='</h2>             
+    $codigo.=$row[4] . "";
+    $codigo.='</h2>             
             <h2 style="width:379px;height:13px;">';
-            $codigo.=$row[34]." ".$row[35] ;
-            $codigo.='</h2>                
+    $codigo.=$row[34] . " " . $row[35];
+    $codigo.='</h2>                
             </div>
             <div id="medio1C">
             <h2>Cliente</h2>                                    
@@ -162,14 +163,14 @@ session_start();
             </div>
             <div id="medio2C">
             <h2>';
-            $codigo.=$row[22]." ";
-            $codigo.='</h2>                                    
+    $codigo.=$row[22] . " ";
+    $codigo.='</h2>                                    
             <h2>';
-            $codigo.=$row[45]." ";
-            $codigo.='</h2>                        
+    $codigo.=$row[45] . " ";
+    $codigo.='</h2>                        
             <h2>';
-            $codigo.= $row[18]." ";
-            $codigo.='</h2>                                  
+    $codigo.= $row[18] . " ";
+    $codigo.='</h2>                                  
             </div>
             <div id="medio3C">
             <h2>Fecha Entrada</h2>                                    
@@ -179,17 +180,17 @@ session_start();
             </div>
             <div id="medio4C">
             <h2>';
-            $codigo.=$row[9]." ";
-            $codigo.='</h2>                                    
+    $codigo.=$row[9] . " ";
+    $codigo.='</h2>                                    
             <h2>';
-            $codigo.=$row[11]." ";
-            $codigo.='</h2>                        
+    $codigo.=$row[11] . " ";
+    $codigo.='</h2>                        
             <h2>';
-            $codigo.=$row[16]." ";
-            $codigo.='</h2>  
+    $codigo.=$row[16] . " ";
+    $codigo.='</h2>  
              <h2>';
-            $codigo.=$row[26]." ";
-            $codigo.='</h2>                             
+    $codigo.=$row[26] . " ";
+    $codigo.='</h2>                             
             </div>
             <div id="acceC">
                 <h2>Accesorios</h2>                                                                                         
@@ -198,15 +199,15 @@ session_start();
             </div>
             <div id="acce1C">            
                 <textarea name="">';
-                $codigo.=$row[6]." ";
-                $codigo.='</textarea>
+    $codigo.=$row[6] . " ";
+    $codigo.='</textarea>
                 <textarea name="">';
-			$codigo.=$row[5]." ";
-                 $codigo.='</textarea>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;El diagnóstico del equipo tiene un precio mínimo de $10.00 (Diez Dólares)</h3>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Toda máquina reparada y no retirada en tres meses será subastada</h3>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Favor revisar que en su orden consten todos los componentes que usted deja. No se admitiran reclamos posteriores</h3>';
-                $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;PyS Systems no se responsabiliza por la pérdida de la información</h3>
+    $codigo.=$row[5] . " ";
+    $codigo.='</textarea>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;El diagnóstico del equipo tiene un precio mínimo de $10.00 (Diez Dólares)</h3>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Toda máquina reparada y no retirada en tres meses será subastada</h3>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;Favor revisar que en su orden consten todos los componentes que usted deja. No se admitiran reclamos posteriores</h3>';
+    $codigo.='<h3 style="width:800px;margin:0px;padding:0px;font-size:11px;font-weight:normal;">&nbsp;&nbsp;PyS Systems no se responsabiliza por la pérdida de la información</h3>
 
             </div>
             <div>
@@ -220,22 +221,22 @@ session_start();
             </div>    
             <div id="respC">
             <h2>';
-             $codigo.="Entrege conforme: ".$row[34]." ".$row[35] ;
-             $codigo.='</h2>                          
+    $codigo.="Entrege conforme: " . $row[34] . " " . $row[35];
+    $codigo.='</h2>                          
             </div>   
             <div id="resp1C">
-            <h2>';             
-             $codigo.="Recibi conforme: ".$row[22];
-             $codigo.='</h2>             
-            </div></div></body>';  
-        }                        
-        
-$codigo=utf8_decode($codigo);
-$dompdf= new DOMPDF();
+            <h2>';
+    $codigo.="Recibi conforme: " . $row[22];
+    $codigo.='</h2>             
+            </div></div></body>';
+}
+
+$codigo = utf8_decode($codigo);
+$dompdf = new DOMPDF();
 $dompdf->load_html($codigo);
-ini_set("memory_limit","32M");
-$dompdf->set_paper("A4","portrait");    
+ini_set("memory_limit", "32M");
+$dompdf->set_paper("A4", "portrait");
 $dompdf->render();
 //$dompdf->stream("reporteRegistro.pdf");
-$dompdf->stream('reporteRegistro.pdf',array('Attachment'=>0));
+$dompdf->stream('reporteRegistro.pdf', array('Attachment' => 0));
 ?>
